a00d7d3ac3d9d80db9ab68e23b8412ed1007a7f8
Ky <ky@noreply.codeberg.org>
2025-08-03 02:32:56 +0200

    Change DPI mode

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace MakeYourChoice
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
